a5e39e475268893ca148eec1d8fde8d7eb4a1f5d
NoushinRasti <63014746+NoushinRasti@users.noreply.github.com>
2020-04-27 01:29:12 +0100

    add scan rate legend and figure2 (log ascale of current dansity versus volatage)

--- same_electrode_size.m
+++ same_electrode_size.m
@@ -13,10 +13,17 @@ for index=1:length(files)
     cell_files{index}.I=data(:,4);
     cell_files{index}.t=data(:,5);
     cell_files{index}.D=GetElectrodeDiameter(strcat(files(index).folder,'\',files(index).name));
+    plot_IV(cell_files{index}.v,cell_files{index}.I,cell_files{index}.r,cell_files{index}.D)
     
-    plot_IV(cell_files{index}.v,cell_files{index}.I,cell_files{index}.r,cell_files{index}.t,cell_files{index}.D)
-    %legend( index, 'Location','north')
 end  
+
+
+cell_legend=cell(length(files),1);
+for i=1:length(files)
+    cell_legend{i}=scan_rate(cell_files{i}.v,cell_files{i}.t);
+end
+legend(cell_legend,'location','northwest')
+hold on
 
 function c = GetElectrodeDiameter(namefile)
 a=extractBefore(namefile,'mm');
@@ -25,14 +32,21 @@ b(2)='.';
 c=str2double(b);
 end
 
-%function scanrate=calculatescanrate(
-%scan_rate=string(round((v(1)-v(index_max))/(t(1)-t(index_max)),3));
-%str=append('scan rate = ',scan_rate,' V/s');
-%end
+function str=scan_rate(v,t)
+
+for i=1:length(v)
+    if ( v(i)>v(i+1)&& v(i+1)>v(i+2))
+        index_max=i; 
+        break;
+    end
+
+scan_rate=string(round((v(1)-v(index_max-2))/(t(1)-t(index_max-2)),3));
+str=strcat('scan rate = ',scan_rate,' V/s');
+
+end
 
 
-
-function plot_IV(v,I,r,t,D) 
+function plot_IV(v,I,r,D) 
 
 counter=0;
 for i=1:length(r)
@@ -44,13 +58,6 @@ for i=1:length(r)
 end
 
 
-for i=1:counter
-    if ( v(i)>v(i+1))
-        index_max=i; 
-        break;
-    end
-end
-
 v=v(1:counter);
 I=I(1:counter);
 
@@ -61,13 +68,8 @@ A= 10^-2* pi*(D/2)^2;
 I_dens=10^3*I./A ; %change to Current density mA.cm^-2
 
 
-
-
-
 figure(1)
 plot(v,I_dens,'LineWidth',3)
-
-
 
  xlim([-2.2 2.2])
 title('ITO/MAPbI(500nm)/Al')
@@ -76,19 +78,16 @@ ylabel('Current Density(mA.cm^-3)')
 
 saveas(gcf,'Figures\0p05mm-2v-cc-3-long-1r.bmp') 
 
+hold on
 
-% figure(2)
-% semilogy(x,abs(y),'-o',...
-%     'LineWidth',3,...
-%     'MarkerSize',3,...
-%     'MarkerEdgeColor','c',...
-%     'MarkerFaceColor',[0.5,0.5,0.5])
-% 
-%  xlim([-2.2 2.2])
-% title('ITO/MAPbI(500nm)/Al')
-% xlabel('Voltage(V)')
-% ylabel('Current Density(mA.cm^-3)')
-% saveas(gcf,'0p05mm-2v-cc-3-long-1r.bmp')
+ figure(2)
+ semilogy(v,abs(I_dens),'LineWidth',3)
+
+  xlim([-2.2 2.2])
+ title('ITO/MAPbI(500nm)/Al')
+ xlabel('Voltage(V)')
+ ylabel('Current Density(mA.cm^-3)')
+ saveas(gcf,'Figures\0p05mm-2v-cc-3-long-1r.bmp')
     
     hold on
 end
